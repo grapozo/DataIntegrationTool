2beab89e26ca2752a9ce2c39fbcfa647d9c3059a
Gabriel Rapozo <ggrapozo@gmail.com>
2022-06-09 20:05:51 -0300

    Updating prettier configuration to use spaces instead of tabs

--- configuration/ts-definitions/definitions.d.ts
+++ configuration/ts-definitions/definitions.d.ts
@@ -1,11 +1,11 @@
 // Handles svg files as Functional components
 declare module '*.svg' {
-	const svgComponent: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
-	export default svgComponent;
+  const svgComponent: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+  export default svgComponent;
 }
 
 // Handles scss files
 declare module '*.scss' {
-	const scssFile: Record<string, string>;
-	export default scssFile;
+  const scssFile: Record<string, string>;
+  export default scssFile;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,58 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-	// If no env variable is set, this fallbacks to "production"
-	mode: process.env.NODE_ENV,
-	entry: {
-		main: './src/index.tsx',
-	},
-	module: {
-		rules: [
-			/**
-			 * Handles tsx files using babel loader.
-			 */
-			{
-				test: /\.tsx?$/,
-				include: path.join(__dirname, 'src'),
-				use: 'babel-loader',
-			},
-			/**
-			 * Handles scss files using sass-loader,
-			 * allows css-loader to modularize it and
-			 * then injects it into the DOM with style-loader.
-			 */
-			{
-				test: /\.s[ac]ss$/i,
-				use: [
-					'style-loader',
-					{
-						loader: 'css-loader',
-						options: {
-							importLoaders: 1,
-							modules: true,
-						},
-					},
-					{
-						loader: 'sass-loader',
-					},
-				],
-			},
-			/**
-			 * Handles .svg files as React Components.
-			 */
-			{
-				test: /\.svg$/,
-				use: ['@svgr/webpack'],
-			},
-		],
-	},
-	plugins: [
-		/**
-		 * Handles HTML files and injects the script bundles.
-		 */
-		new HtmlWebpackPlugin({
-			filename: './index.html',
-			template: './public/index.html',
-		}),
-	],
+  // If no env variable is set, this fallbacks to "production"
+  mode: process.env.NODE_ENV,
+  entry: {
+    main: './src/index.tsx'
+  },
+  module: {
+    rules: [
+      /**
+       * Handles tsx files using babel loader.
+       */
+      {
+        test: /\.tsx?$/,
+        include: path.join(__dirname, 'src'),
+        use: 'babel-loader'
+      },
+      /**
+       * Handles scss files using sass-loader,
+       * allows css-loader to modularize it and
+       * then injects it into the DOM with style-loader.
+       */
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true
+            }
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      },
+      /**
+       * Handles .svg files as React Components.
+       */
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack']
+      }
+    ]
+  },
+  plugins: [
+    /**
+     * Handles HTML files and injects the script bundles.
+     */
+    new HtmlWebpackPlugin({
+      filename: './index.html',
+      template: './public/index.html'
+    })
+  ]
 };
