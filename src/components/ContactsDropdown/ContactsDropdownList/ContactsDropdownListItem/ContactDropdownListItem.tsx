c545ac5c9aeea9ba620c4b90a8e5ef0eb6f15fa7
Gabriel Rapozo <ggrapozo@gmail.com>
2022-06-12 00:34:15 -0300

    Add ContactDropdownListItem docs and tests

--- src/components/ContactsDropdown/ContactsDropdownList/ContactsDropdownListItem/ContactDropdownListItem.tsx
+++ src/components/ContactsDropdown/ContactsDropdownList/ContactsDropdownListItem/ContactDropdownListItem.tsx
@@ -1,10 +1,34 @@
+import { useState } from 'react';
+import { Contact } from 'shared/interfaces/contact.interface';
 import styles from './ContactsDropdownListItem.scss';
 
-export const ContactDropdownListItem = (): JSX.Element => {
+type ContactsDropdownListItemProps = {
+  contact: Contact;
+  onContactSelectCallback: (contact: Contact, contactIsSelected: boolean) => void;
+};
+
+/**
+ * A simple checkbox which renders the Contact information.
+ */
+export const ContactDropdownListItem = ({
+  contact,
+  onContactSelectCallback
+}: ContactsDropdownListItemProps): JSX.Element => {
+  const [checkboxSelected, setCheckboxSelected] = useState(false);
+
+  /**
+   * Sets the selected state and notifies the parent component.
+   */
+  const handleContactSelect = (): void => {
+    setCheckboxSelected(!checkboxSelected);
+    onContactSelectCallback(contact, !checkboxSelected);
+  };
+
   return (
     <div className={`${styles.contactDropdownListItem}`}>
       <label>
-        <input type="checkbox" />
+        <input type="checkbox" checked={checkboxSelected} onChange={handleContactSelect} />
+        {contact.name}
       </label>
     </div>
   );
