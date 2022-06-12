46854df8d6a7900d0de27698543186d7c0b8d8da
Gabriel Rapozo <ggrapozo@gmail.com>
2022-06-12 13:48:28 -0300

    Add ContactSyncCard Component and tests

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,9 +1,23 @@
-import { ContactsDropdown } from '../components/ContactsDropdown';
-import { SyncContactsButton } from '../components/SyncContactsButton';
+import { generateListOfContacts } from '../components/ContactsDropdown/helpers';
+import { ContactSyncCard } from '../components/ContactSyncCard';
+import GoogleIcon from './assets/GoogleIcon.svg';
+import MailchimpIcon from './assets/MailchimpIcon.svg';
+
 export const App = (): JSX.Element => {
   return (
     <div>
-      <ContactsDropdown />
+      <ContactSyncCard
+        ServiceIcon={GoogleIcon}
+        serviceName={'Gmail'}
+        serviceDescription={'These Gmail contacts will sync to MailChimp'}
+        contacts={generateListOfContacts(10)}
+      />
+      <ContactSyncCard
+        ServiceIcon={MailchimpIcon}
+        serviceName={'Mailchimp'}
+        serviceDescription={'These Mailchimp contacts will sync to Gmail'}
+        contacts={generateListOfContacts(10)}
+      />
     </div>
   );
 };
